37a7387e71a93e7e4ce24374b84f49cb52aa1bf5
barispolatt <barispolat1999@hotmail.com>
2025-04-23 21:57:11 +0300

    Created create-user.dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Name is required' })
+  name: string;
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail({}, { message: 'Email is required' })
+  email: string;
+  @IsNotEmpty({ message: 'Password is required' })
+  @IsStrongPassword({}, { message: 'Password is weak' })
+  password: string;
+  @IsOptional()
+  birthdate: string;
+}
