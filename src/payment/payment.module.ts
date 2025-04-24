70044a3cd1115500d4b9db38f3b10b8008d1928f
barispolatt <barispolat1999@hotmail.com>
2025-04-24 22:44:17 +0300

    Created: - ProductsModule (Feature): Service & Controller - OrderModule (Feature): Service & Controller - PaymentModule (Feature): Service & Controller - Auth Module (Global): Service

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PaymentController } from './controller/payment.controller';
+import { PaymentService } from './service/payment.service';
 
-@Module({})
+@Module({
+  controllers: [PaymentController],
+  providers: [PaymentService],
+})
 export class PaymentModule {}
