1fc9850a71ed1ad7aaa51375c8a02d1828d9000d
barispolatt <barispolat1999@hotmail.com>
2025-04-25 14:27:24 +0300

    super-admin.guard.ts nad admin.guard.ts files are updated.

--- src/auth/guard/admin.guard.ts
+++ src/auth/guard/admin.guard.ts
@@ -1,11 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
+import { Reflector } from '@nestjs/core';
+import { dummyUsers } from '../../common/utils/data';
+import { UserRole } from '../../common/utils/types';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private reflector: Reflector,
+  ) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const user = dummyUsers[0];
+    return user.role === UserRole.ADMIN;
   }
 }
--- src/auth/guard/super-admin.guard.ts
+++ src/auth/guard/super-admin.guard.ts
@@ -2,9 +2,16 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { dummyUsers } from '../../common/utils/data';
 import { UserRole } from '../../common/utils/types';
+import { Reflector } from '@nestjs/core';
+import { AuthService } from '../service/auth.service';
 
 @Injectable()
 export class SuperAdminGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private reflector: Reflector,
+  ) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
