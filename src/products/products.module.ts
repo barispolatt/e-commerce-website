9b7ebcecf0b31b1ec618c24009f5a63eb62e9502
barispolatt <barispolat1999@hotmail.com>
2025-05-02 00:03:04 +0300

    product.module.ts updated

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './controller/products.controller';
 import { ProductsService } from './service/products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService],
