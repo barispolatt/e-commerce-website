12c907ef85c1c351bedc5bc83e2abb28ea12647b
barispolatt <barispolat1999@hotmail.com>
2025-05-02 00:01:27 +0300

    product.entity.ts created.

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,21 @@
+import { BaseEntityWithName } from '../../common/entities/BaseEntityWithName';
+import { Column } from 'typeorm';
+
+export class Product extends BaseEntityWithName {
+  @Column({ type: 'varchar', length: 255, unique: true })
+  description: string;
+
+  @Column({ type: 'decimal', precision: 8, scale: 2 })
+  price: number;
+
+  @Column({ type: 'int', default: 0 })
+  stock: number;
+
+  @Column({ type: 'boolean', default: false })
+  isActive: boolean;
+
+  constructor(productDTO: Partial<Product>) {
+    super();
+    Object.assign(this, { ...productDTO });
+  }
+}
