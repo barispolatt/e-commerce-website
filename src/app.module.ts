6161e11026a8bdcc28f9822ee1808e3768558013
barispolatt <barispolat1999@hotmail.com>
2025-05-01 20:04:12 +0300

    docker-compose file updated. TypeORM packages installed.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { OrderModule } from './order/order.module';
 import { PaymentModule } from './payment/payment.module';
@@ -8,6 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptor/response.interceptor';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
