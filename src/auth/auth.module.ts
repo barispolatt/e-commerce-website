3708f34ff190e8f72e68763c188e48b86b51fc07
barispolatt <barispolat1999@hotmail.com>
2025-05-06 21:32:31 +0300

    JwtStrategy.ts file updated.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,26 @@ import { AuthService } from './service/auth.service';
 import { SuperAdminGuard } from './guard/super-admin.guard';
 import { AdminGuard } from './guard/admin.guard';
 import { AuthController } from './controller/auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './jwt/JwtStrategy';
 
 @Global()
 @Module({
-  providers: [AuthService, SuperAdminGuard, AdminGuard],
+  imports: [
+    UsersModule,
+    ConfigService,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET')!,
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
+  providers: [AuthService, SuperAdminGuard, AdminGuard, JwtStrategy],
   exports: [AuthService, SuperAdminGuard, AdminGuard],
   controllers: [AuthController],
 })
