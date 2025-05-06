5ff130a304121b9ffed0a052ba92523b6fcedcbd
barispolatt <barispolat1999@hotmail.com>
2025-05-06 21:27:55 +0300

    JwtStrategy.ts file created.

---
+++ src/auth/jwt/JwtStrategy.ts
@@ -0,0 +1,30 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../../users/service/users.service';
+import { ConfigService } from '@nestjs/config';
+import { UserRole } from '../../common/utils/types';
+
+interface JwtPayload {
+  sub: string;
+  email: string;
+  role: UserRole;
+}
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly config: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get<string>('JWT_SECRET') || '',
+    });
+  }
+  async validate(payload: JwtPayload) {
+    const user = await this.usersService.findOne(Number(payload.sub));
+    if (!user) throw new UnauthorizedException();
+    return user;
+  }
+}
