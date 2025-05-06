904d5c54e8ac6424aa54ce3b0501ba082a2d9f0e
barispolatt <barispolat1999@hotmail.com>
2025-05-06 21:38:09 +0300

    auth.service.ts updated. login.dto.ts created.

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { UserRole } from '../../common/utils/types';
+import { UsersService } from '../../users/service/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../../users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  validateUserRole(userRole: UserRole, requiredRole: UserRole): boolean {
-    return userRole <= requiredRole;
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<Partial<User> | null> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && password === user.password) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
   }
 }
