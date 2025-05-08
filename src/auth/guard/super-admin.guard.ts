2154ab77c58fd135b82f797ea6107d9f12d8dfa5
barispolatt <barispolat1999@hotmail.com>
2025-05-08 17:45:41 +0300

    super-admin.guard.ts updated

--- src/auth/guard/super-admin.guard.ts
+++ src/auth/guard/super-admin.guard.ts
@@ -1,8 +1,19 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserRole } from '../../common/utils/types';
 import { Reflector } from '@nestjs/core';
 import { AuthService } from '../service/auth.service';
+
+interface RequestUser {
+  role?: UserRole;
+}
+interface RequestWithUser extends RequestUser {
+  user?: RequestUser;
+}
 
 @Injectable()
 export class SuperAdminGuard implements CanActivate {
@@ -10,10 +21,15 @@ export class SuperAdminGuard implements CanActivate {
     private authService: AuthService,
     private reflector: Reflector,
   ) {}
-
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const user = request.user;
+    if (!user) {
+      throw new UnauthorizedException('Unknown user');
+    }
+    if (user.role === UserRole.SUPER_ADMIN) {
+      return true;
+    }
+    throw new UnauthorizedException('Unauthorized user, needs SuperAdmin');
   }
 }
